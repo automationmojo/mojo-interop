bef91551a337029fd56ee46c32daf7424b6d8a4d
Myron Walker <myron.walker@gmail.com>
2023-06-11 04:37:32 -0700

    Some more copyright additions

--- source/packages/mojo/interop/clusters/constants.py
+++ source/packages/mojo/interop/clusters/constants.py
@@ -1,2 +1,18 @@
+"""
+.. module:: constants
+    :platform: Darwin, Linux, Unix, Windows
+    :synopsis: Contains the cluster related constants.
+
+.. moduleauthor:: Myron Walker <myron.walker@gmail.com>
+"""
+
+__author__ = "Myron Walker"
+__copyright__ = "Copyright 2023, Myron W Walker"
+__credits__ = []
+__version__ = "1.0.0"
+__maintainer__ = "Myron Walker"
+__email__ = "myron.walker@gmail.com"
+__status__ = "Development" # Prototype, Development or Production
+__license__ = "MIT"
 
 INTEGRATION_CLASS_FOR_RASPBERRYPI_NODE = "network/raspberrypi-node"
