9a7939473399e1f0c18def6218f7d240e24b51f2
Myron Walker <myron.walker@gmail.com>
2023-11-17 19:25:54 -0600

    Add an example tasking for use with testing in testplus

---
+++ source/packages/mojo/interop/protocols/tasker/examples/helloworldtasking.py
@@ -0,0 +1,39 @@
+
+import os
+import time
+
+from mojo.results.model.progressinfo import ProgressInfo, ProgressType, ProgressCode
+
+from mojo.interop.protocols.tasker.tasking import Tasking
+from mojo.interop.protocols.tasker.taskingresult import TaskingResult
+
+class HelloWorldTasking(Tasking):
+
+    def begin(self, kwparams: dict):
+
+        self._data = {
+            "pid": os.getpid(),
+            "message": kwparams["message"]
+        }
+        
+        time.sleep(5)
+
+        return
+
+    def mark_progress_start(self):
+        self._current_progress = ProgressInfo(self._task_id, ProgressType.NumericRange, self.full_name, ProgressType.NumericRange,
+                                              0, 5, 0, ProgressCode.Running, self._data)
+        return
+
+    def perform(self):
+
+        position = self._current_progress.position
+
+        time.sleep(1)
+
+        morework = False
+        if position < self._current_progress.range_max:
+            morework = True
+            self._current_progress.position = position + 1
+
+        return morework
